5637a02b1cdb497573b5023572bfe8949677fe73
Steve Meyer <stephen.meyer@wisc.edu>
2018-11-28 14:23:55 -0600

    Upgraded to latest Rails 4 minor version

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-
   get 'authors/:lcnafid' => 'authors#view', as: :author
   get 'catalog/:id' => 'catalog#show', as: :catalog
-
-
   root 'application#home'
-
 end
